0d246ab6cdf6bb8db4d1996cba990dd27fc2281f
velmarshal <100859647+velmarshal@users.noreply.github.com>
2022-03-31 22:49:21 +0200

    stat rolling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 
+//import EventEmitter from 'events';
 async function bootstrap() {
     const app = express();
 
@@ -11,3 +12,42 @@ async function bootstrap() {
 }
 
 bootstrap().catch(e => console.log(e));
+console.log('test');
+
+//separator
+
+//stat generator
+const StatNames = ["Strength", "Dexterity", "Constitution","Intelligence","Wisdom","Charisma"];
+let StatValues: number [] = new Array<number>(5);
+let RollResults: number[];
+
+function generateStats(): void {
+    for (let i = 0; i<6; i++){
+        let roll: number = 0;
+        let j: number = 0;
+        let droppedroll: number = 0;
+        RollResults = [19, 19, 19];
+        for (let k: number = 0; k<4; k++){
+            roll = Math.ceil(Math.random()*5)+1;
+            console.log(`Rolling ${roll}`);
+            if (droppedroll === 0) {
+            droppedroll = roll;
+            } else if (roll < droppedroll) {
+                RollResults [j] = droppedroll;
+                j++;
+                droppedroll = roll;
+            } else {
+                RollResults [j] = roll;
+                j++;
+            };
+            console.log(`dropped roll is ${droppedroll}`);
+        };
+        let rollsum = RollResults[0] + RollResults[1] + RollResults[2];
+        StatValues[i] = rollsum;
+        
+        console.log(`Character's ${StatNames[i]} is equal to ${StatValues[i]}`);
+    };
+    
+};
+
+generateStats();
